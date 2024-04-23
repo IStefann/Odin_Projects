d187170175c0b5c49c7adfcdb2e357562b3d40bb
IStefann <118936693+IStefann@users.noreply.github.com>
2024-04-24 00:32:43 +0200

    GUI Finished

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,9 +1,13 @@
-console.log("First to 5 points wins!");
 let computerChoice;
 let playerChoice;
 let roundsPlayed = 0;
 let pcPoints = 0;
 let playerPoints = 0;
+const buttonListener = document.querySelectorAll(".btn");
+const result = document.querySelector(".result");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".pcScore");
+
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
@@ -18,36 +22,51 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    do
-        playerChoice = prompt("Write Rock, Paper or Scissors").toLowerCase();
-    while (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors");
+    buttonListener.forEach(element => {
+        element.addEventListener("click", () => {
+            switch (element.id) {
+                case '1': playerChoice = "rock"; break;
+                case '2': playerChoice = "paper"; break;
+                case '3': playerChoice = "scissors"; break;
+            }
+            playRound();
+        })
+    });
 }
-
 function playRound() {
+    if(pcPoints === 5 || playerPoints === 5)
+    {
+        pcPoints = 0;
+        playerPoints = 0;
+        playerScore.textContent = `Player's score: ${playerPoints}`;
+        computerScore.textContent = `Computer's score: ${pcPoints}`;
+    }
     getComputerChoice();
-    getPlayerChoice();
     if (computerChoice === playerChoice)
-        console.log("Tie! - No points awarded");
+        result.textContent = "Tie! - No points awarded";
     else if (
         (computerChoice === "rock" && playerChoice === "scissors") ||
         (computerChoice === "paper" && playerChoice === "rock") ||
         (computerChoice === "scissors" && playerChoice === "paper")
     ) {
-        console.log(`Computer gets the point! - Computer chose: ${computerChoice}, whilst the player chose: ${playerChoice}`);
+        result.innerHTML = `Computer Chose: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}, <span style="color:red;">It gains a point</span>`;
         pcPoints++;
-    } else {
-        console.log(`Player gets the point! - Computer chose: ${computerChoice}, whilst the player chose: ${playerChoice}`);
-        playerPoints++;
-    }
-}
+        computerScore.textContent = `Computer's score: ${pcPoints}`;
 
-while (true) {
-    playRound();
-    if (pcPoints === 5) {
-        console.log("PC WINS!");
-        break;
-    } else if (playerPoints === 5) {
-        console.log("PLAYER WINS!");
-        break;
+    } else {
+        result.innerHTML = `Computer Chose: ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}, <span style="color:green;">You gain a point</span>`;
+        playerPoints++;
+        playerScore.textContent = `Player's score: ${playerPoints}`;
     }
+    if(playerPoints === 5)
+    {
+        result.innerHTML = 'You <span style="color:green;">win!</span>, good job!';
+
+    }
+    else if(pcPoints === 5)
+    {
+        result.innerHTML = 'You <span style="color:red;">Lose!</span> :(';
+    }
+    
 }
+getPlayerChoice();
