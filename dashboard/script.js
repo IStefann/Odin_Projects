da897f268a96f12d86f786b0dd3f259b478a4d49
IStefann <118936693+IStefann@users.noreply.github.com>
2024-05-14 23:40:08 +0200

    Complete site for desktop

---
+++ dashboard/script.js
@@ -0,0 +1,7 @@
+const noSold = document.querySelectorAll("#noSold");
+const price = document.querySelectorAll("#price");
+const total = document.querySelectorAll("#total");
+
+noSold.forEach(element => {
+    
+});
