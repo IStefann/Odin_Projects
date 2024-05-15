015a5ac0785c12bb92b1f567c3413daac0adfb87
IStefann <118936693+IStefann@users.noreply.github.com>
2024-05-15 14:15:45 +0200

    Fix background scrolling while sidebar is open

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,12 +1,15 @@
 const open = document.querySelector(".hamburger");
 const close = document.querySelector("#close");
-let sidebar = document.querySelector(".sidebar");
+const sidebar = document.querySelector(".sidebar");
+const body = document.querySelector("body");
 
 open.addEventListener("click", () => {
+	body.style.position = "fixed";
 	sidebar.classList.remove("off");
 	sidebar.classList.add("on");
 });
 close.addEventListener("click", () => {
+	body.style.position = "";
 	sidebar.classList.remove("on");
 	sidebar.classList.add("off");
 });
